dc2bca7f552330ac6e5c7e0e304d32371eeb1d16
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-14 21:44:32 +0530

    Refactor Pages controller: comment out debug message and add index and about methods

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,7 +2,14 @@
    class Pages{
     public function __construct()
     {
-        echo "this is my name is ramesh peshala";
+        // echo "this is my name is ramesh peshala";
+    }
+
+    public function index(){
+
+    }
+    public function about($name){
+           echo 'hi ,'.$name;
     }
    }
 ?>
