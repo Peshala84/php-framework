a540398a9dc804ec510411ffbf308b61f590c52d
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-15 22:27:22 +0530

    Refactor Pages controller to utilize M_pages model for user data retrieval in about method

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,18 +1,18 @@
 <?php
    class Pages extends Controller{
+    private $pagesModel;
     public function __construct()
     {
-        // echo "this is my name is ramesh peshala";
-
+        $this->pagesModel = $this->model('M_pages');
     }
 
     public function index(){
             
     }
-    public function about($name, $age){
+    public function about(){
+        $users = $this->pagesModel->getUsers();
         $data = [
-            'username' => $name,
-            'userage' => $age
+            'users' => $users
         ];
         $this->view('v_about' , $data);
     }
