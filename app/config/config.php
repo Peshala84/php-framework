5f0bdcb017b72f14a05d9cc2915d596a88296fc8
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-15 08:58:43 +0530

    Add configuration and controller classes for application structure

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+//APP ROOT
+  define('APPROOT', dirname(dirname(__FILE__)));  
+
+  //URL ROOT
+  define('URLROOT', 'http://localhost/FrameWork');
+
+    //SITE NAME
+    define('SITENAME', 'FrameWork');
+?>
