901fb98ad4ec859d2db467078b034074fef12773
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-15 22:27:12 +0530

    Add database configuration settings to config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,12 @@
 <?php
 
+//database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'framework');
+
+
 //APP ROOT
   define('APPROOT', dirname(dirname(__FILE__)));  
 
